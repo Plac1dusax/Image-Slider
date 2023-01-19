6ebe39ea95073686861ee4e6058679cbb5ea2bff
Furkan <FSDeryol@gmail.com>
2023-01-19 21:16:52 +0300

    images added, arrow buttons and navigation buttons added, project finished

--- src/App.js
+++ src/App.js
@@ -1,13 +1,63 @@
-import React from "react"
-import { Database } from "./components/Database"
+import React, { useState } from "react"
+import { Database } from "./components/Database/Database"
+import { FiChevronLeft } from "react-icons/fi"
+import { FiChevronRight } from "react-icons/fi"
+import { BsFillCircleFill } from "react-icons/bs"
 import "./App.css"
 
 function App() {
+  const [currentIndex, setCurrentIndex] = useState(0)
+  const length = Database.length
+
+  function handleSlideLeft() {
+    const isLastImage = currentIndex === 0
+
+    isLastImage
+      ? setCurrentIndex(Database.length - 1)
+      : setCurrentIndex(currentIndex - 1)
+  }
+
+  function handleSlideRight() {
+    const isLastImage = currentIndex === Database.length - 1
+
+    isLastImage ? setCurrentIndex(0) : setCurrentIndex(currentIndex + 1)
+  }
+
+  function handleNavigation(e) {
+    const selected = e.target.dataset.index
+
+    setCurrentIndex(parseInt(selected))
+  }
+
   return (
     <div className="image-slider-container">
       <div className="image-slider">
-        {Database.map(data => {
-          return <img src={data.imageLink} />
+        <div className="arrow-icon arrow-left" onClick={handleSlideLeft}>
+          <FiChevronLeft />
+        </div>
+        <div className="arrow-icon arrow-right" onClick={handleSlideRight}>
+          <FiChevronRight />
+        </div>
+        <div className="navigate">
+          {Database.map((data, index) => {
+            return (
+              <BsFillCircleFill
+                key={index}
+                data-index={index}
+                className={index === currentIndex ? "selected" : null}
+                onClick={handleNavigation}
+              />
+            )
+          })}
+        </div>
+        {Database.map((data, index) => {
+          return (
+            <img
+              src={data.imageLink}
+              key={index}
+              className={currentIndex === index ? "active" : null}
+            />
+          )
         })}
       </div>
     </div>
